cd51b0f7a27ee6cc556d9b65db09707f56f910a6
lucymenefee <71661897+lucymenefee@users.noreply.github.com>
2020-10-21 19:59:43 -0700

    update

--- p5js/sketch3/sketch.js
+++ p5js/sketch3/sketch.js
@@ -1,7 +1,15 @@
 function setup() {
-    createCanvas(400, 400);
-  }
-  
-  function draw() {
-    background(220);
-  }
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background(150);
+  for (var y = 0; y <= height; y += 50) {
+    for (var x = 0; x <= width; x += 50) {
+    fill(300, 300, 300);
+    circle(x, y, 30);
+    fill(255, 0, 0);
+    line(x, y, 300, 300)
+}
+}
+}
